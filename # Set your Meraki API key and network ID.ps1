1873cf34595756d70d8ae14c3fdc15631f1e6bce
Harrison <47501747+Code-acus@users.noreply.github.com>
2023-04-27 13:31:34 -0600

    Create # Set your Meraki API key and network ID.ps1

---
+++ # Set your Meraki API key and network ID.ps1
@@ -0,0 +1,59 @@
+# Set your Meraki API key and network ID
+$apiKey = "your-api-key"
+$networkId = "your-network-id"
+
+# Set the endpoint URL for retrieving network status information
+$url = "https://api.meraki.com/api/v0/networks/$networkId/devices/statuses"
+
+# Set the alternate path IP address
+$altPath = "alternate-path-ip-address"
+
+# Set the original path IP address
+$originalPath = "original-path-ip-address"
+
+# Set the headers for the API request
+$headers = @{
+    "X-Cisco-Meraki-API-Key" = $apiKey
+}
+
+# Loop until the script is stopped
+while ($true) {
+    try {
+        # Make the API request
+        $response = Invoke-RestMethod -Uri $url -Headers $headers
+
+        # Parse the response to extract outage information
+        $outages = $response | Where-Object {$_.status -ne "ok"}
+
+        # Determine the best alternate path to use based on the outage information
+        # and set the new route in the routing table
+        if ($outages) {
+            # Set the new route in the routing table
+            New-NetRoute -DestinationPrefix "0.0.0.0/0" -InterfaceIndex 1 -NextHop $altPath
+        }
+        else {
+            # Set the new route in the routing table
+            New-NetRoute -DestinationPrefix "0.0.0.0/0" -InterfaceIndex 1 -NextHop $originalPath
+        }
+
+        # Loop until the original path becomes available again
+        while ($true) {
+            # Determine if the original path is available
+            $pingResult = Test-Connection -ComputerName $originalPath -Count 1 -Quiet
+
+            # If the original path is available, switch back to it and exit the loop
+            if ($pingResult) {
+                # Set the new route in the routing table
+                New-NetRoute -DestinationPrefix "0.0.0.0/0" -InterfaceIndex 1 -NextHop $originalPath
+                break
+            }
+
+            # Wait for a short period before checking again
+            Start-Sleep -Seconds 5
+        }
+    }
+    catch {
+        # Handle any errors that occur during the script execution
+        Write-Host "An error occurred: $_"
+    }
+}
